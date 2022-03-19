c0023ea462806ce937f9eddc11ae42fa729ae7e1
Julio Gordijo <jgordijo@gmail.com>
2022-03-18 23:32:03 -0300

    created specifications services

---
+++ src/modules/cars/repositories/SpecifcationsRepository.ts
@@ -0,0 +1,31 @@
+import { Specification } from '../model/Specification';
+import { ISpecificationsRepository, ICreateSpecificationsDTO } from './ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationsDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string) {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
